59b7b4dd1473f027fe33ed85bcccd33b699ffa78
Shane <shane.forbrigger@queensu.ca>
2020-06-17 14:59:00 -0400

    Updated animation script to use mat file and timeseries

--- teleop_vector_animation.m
+++ teleop_vector_animation.m
@@ -1,26 +1,33 @@
 % teleop_vector_animation.m
 close all;
-max_mag = sqrt(max([sum(velocity_patient.signals.values.*...
-                       velocity_patient.signals.values,2);
-                   sum(f_th_d.signals.values.*f_th_d.signals.values,2);
-                   sum(f_mod.signals.values.*f_mod.signals.values,2)]));
+clear;
+data_dir = "E:/Users/shfor/OneDrive - Queen's University/sim_data_and_outputs/intent_preserving_teleop/output_data/";
+data_file = "20200617T105638_teleop_sim_angle";
+load(strcat(data_dir, data_file));
                
 f_factor = 0.1;
 
-plot_times = 1:25:length(velocity_patient.time);
+frame_time = 0.05;
+slowmo_factor = 0.5;
+
+plot_times = 0:frame_time*slowmo_factor:max(velocity_patient.Time);
+
+f_th_d = resample(f_th_d, plot_times);
+f_mod = resample(f_mod, plot_times);
+position_patient = resample(position_patient, plot_times);
+velocity_patient = resample(velocity_patient, plot_times);
 
 % Setting up video parameters
-frame_time = 0.05;
 v = VideoWriter('force_rotation.mp4','MPEG-4');
 v.FrameRate = 1/frame_time;
 F(length(plot_times)) = struct('cdata',[],'colormap',[]);
 
 h_fig = figure('visible','off');
-plot(position_desired.signals.values(:,1),...
-     position_desired.signals.values(:,2));
+plot(position_desired.Data(:,1),...
+     position_desired.Data(:,2));
 xlabel('X Position [m]');
 ylabel('Y Position [m]');
-title('Rotation - 50 ms delay (0.5x speed)')
+title(['Rotation - ' num2str(wrkspcVars.T) ' ms delay (' num2str(slowmo_factor) 'x speed)'])
 hold on;
 h_axis = gca;
 axis equal
@@ -32,15 +39,15 @@ f_line = line([0 1],[0 1],'Color','b');
 f_mod_line = line([0 0],[0 1],'Color','g');
 v.open();
 try
-    for i = plot_times
-        update_vector(f_line,-f_factor*f_th_d.signals.values(i,1:2));
-        move_vector(f_line,position_patient.signals.values(i,1:2));
-    %     update_vector(f_a_line,f_a.signals.values(i,1:2));
-    %     update_vector(f_g_line,f_g.signals.values(i,1:2));
-        update_vector(f_mod_line,-f_factor*f_mod.signals.values(i,1:2));
-        move_vector(f_mod_line,position_patient.signals.values(i,1:2));
-        update_vector(v_line,f_factor*velocity_patient.signals.values(i,1:2));
-        move_vector(v_line,position_patient.signals.values(i,1:2));
+    for i = 1:length(plot_times)
+        update_vector(f_line,-f_factor*f_th_d.Data(i,1:2));
+        move_vector(f_line,position_patient.Data(i,1:2));
+    %     update_vector(f_a_line,f_a.Data(i,1:2));
+    %     update_vector(f_g_line,f_g.Data(i,1:2));
+        update_vector(f_mod_line,-f_factor*f_mod.Data(i,1:2));
+        move_vector(f_mod_line,position_patient.Data(i,1:2));
+        update_vector(v_line,f_factor*velocity_patient.Data(i,1:2));
+        move_vector(v_line,position_patient.Data(i,1:2));
         drawnow
         F(i) = getframe(h_fig);
         v.writeVideo(F(i))
