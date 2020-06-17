947a7a9b4b102185072d5211ce82dc98ebd9a038
Shane <shane.forbrigger@queensu.ca>
2020-06-17 10:46:12 -0400

    Updated scripts to rely on project path rather than hardcoding paths as variables

--- run_model.m
+++ run_model.m
@@ -2,20 +2,20 @@
 clear;
 
 start_dir = pwd;
-output_data_dir = "output_data/";
+output_dir = "E:/Users/shfor/OneDrive - Queen's University/sim_data_and_outputs/intent_preserving_teleop/";
+data_dir = "output_data/";
 model_dir = "models/";
 model_name = 'teleop_sim_angle';
-% Path of the setup script *relative to the model directory*
-setup_script_path = "workspace_setup_scripts/teleop_sim_setup.m";
+setup_script = "teleop_sim_setup.m";
 % It's easier to work in the model's directory temporarily
 cd(model_dir)
-update_model_workspace(model_name, setup_script_path);
+update_model_workspace(model_name, setup_script);
 wrkspcVars = get_model_workspace(model_name);
 sim(model_name);
 cd(start_dir)
 
 % Saving all simulation inputs and outputs, except the directory strings
-save(strcat(output_data_dir, datestr(now,30), "_", model_name),...
+save(strcat(output_dir, data_dir, datestr(now,30), "_", model_name),...
      '-regexp', '^(?!(model_dir|output_data_dir|start_dir|tout)$).');
 
 function update_model_workspace(model_name, script_path)
